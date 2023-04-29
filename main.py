9a8dc60fa649d1cb9b7d54abdb296d151fd4e8cb
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-04-29 19:10:46 +0200

    Fix pyqt5 version and add designer.

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 
 from PyQt5.QtWidgets import QApplication
-from main_window_layout.q_main_window import MainWindow
+from PyQt5 import uic
+#from main_window_layout.q_main_window import MainWindow
+from untitled import Ui_MainWindow
+#from main_window_layout.q_resizeable_widget import ResizableLabel
+from PyQt5 import QtWidgets
 import sys
+"""
+class Test(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        layout = QtWidgets.QGridLayout(self)
 
+        for row in range(3):
+            for column in range(3):
+                if (row, column) == (1, 1):
+                    continue
+                layout.addWidget(QtWidgets.QPushButton(), row, column)
+
+        label = ResizableLabel()
+        layout.addWidget(label, 1, 1)
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    w = Test()
+    w.show()
+    sys.exit(app.exec_())
+
+"""
 app = QApplication(sys.argv)
-
-# Create a Qt widget, which will be our window.
-window = MainWindow()
-window.show()  # IMPORTANT!!!!! Windows are hidden by default.
-
-
+window = uic.loadUi('untitled.ui')
+window.show()
 # Start the event loop.
 app.exec()
