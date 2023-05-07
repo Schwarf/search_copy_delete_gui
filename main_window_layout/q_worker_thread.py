2dc4f0824a358b3ccb097e50e4be2378398bccaf
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-07 16:42:20 +0200

    Need a thread pool.

--- main_window_layout/q_worker_thread.py
+++ main_window_layout/q_worker_thread.py
@@ -1,11 +1,11 @@
 import time
 
-from PyQt5.QtCore import QThread, pyqtSignal, QRegExp
+from PyQt5.QtCore import QThread, pyqtSignal
 
 
 class PathSearchThread(QThread):
     finished = pyqtSignal()
-
+    thread_count : int = 0
     def __init__(self, parent=None):
         super().__init__(parent)
         self._running = False
@@ -13,8 +13,9 @@ class PathSearchThread(QThread):
 
     def run(self):
         self._running = True
+        PathSearchThread.thread_count += 1
         while self._running:
-            print("Worker thread is running...")
+            print("Worker thread is running..." + str(PathSearchThread.thread_count))
             time.sleep(1)
         self.finished.emit()
 
