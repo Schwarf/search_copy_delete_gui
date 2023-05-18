e2f54a3a3021b56fa2a7e0a2a5c10b6a0cc2f405
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-18 13:25:28 +0200

    Methods for widgets.

--- main_window_layout/q_main_window.py
+++ main_window_layout/q_main_window.py
@@ -1,15 +1,16 @@
 from typing import Optional
-from PyQt5.QtCore import QSize, pyqtSlot, QThread, QRegExp, QThreadPool
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QLineEdit, QFormLayout, QVBoxLayout, QBoxLayout, \
-    QTextEdit
-from PyQt5.QtGui import QRegExpValidator
 
-from q_layout import MainWindowQVLayout
-from q_worker_thread import PathSearchThread
+from PyQt5.QtCore import pyqtSlot, QThread, QRegExp, QThreadPool
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QLineEdit, QFormLayout, QTextEdit
+
 from q_runnable import PathSearchRunnable
+
+
 # Subclass QMainWindow to customize your application's main window
 class MainWindow(QMainWindow):
     """Main Window"""
+
     def __init__(self):
         super().__init__()
         self._title = 'Search only'
@@ -17,7 +18,7 @@ class MainWindow(QMainWindow):
         self._top = 10
         self._width = 1920
         self._height = 1080
-        self._worker_thread : Optional[QThread] = None
+        self._worker_thread: Optional[QThread] = None
         start_path_regular_expression = QRegExp("[A-Za-z0-9\-\_\/]+")
         sub_path_regular_expression = QRegExp("[A-Za-z0-9\-\_\*\/]+")
         self._start_path_validator = QRegExpValidator(start_path_regular_expression)
@@ -27,30 +28,41 @@ class MainWindow(QMainWindow):
         self._thread_pool = QThreadPool.globalInstance()
         self._thread_count: int = 0
 
+    def _start_path_input(self) -> QLineEdit:
+        start_path_input = QLineEdit(self)
+        start_path_input.move(20, 20)
+        start_path_input.resize(280, 40)
+        start_path_input.setValidator(self._start_path_validator)
+        start_path_input.textChanged.connect(self.run_task)
+        return start_path_input
+
+    def _search_button(self) -> QPushButton:
+        search_button = QPushButton('Search', self)
+        search_button.move(20, 80)
+        search_button.clicked.connect(self.on_click)
+        return search_button
+
+    def _output_text_box(self) -> QTextEdit:
+        output_text_box = QTextEdit(self)
+        output_text_box.move(120, 120)
+        output_text_box.resize(880, 140)
+        output_text_box.setReadOnly(True)
+        return output_text_box
+
     def init_ui(self):
         """Window Geometry"""
         widget = QWidget()
         self.setWindowTitle(self._title)
         self.setGeometry(self._left, self._top, self._width, self._height)
-        #label
+        # label
         # Create textbox
-        self._start_path_input = QLineEdit(self)
-        self._start_path_input.move(20, 20)
-        self._start_path_input.resize(280, 40)
-        self._start_path_input.setValidator(self._start_path_validator)
-        self._start_path_input.textChanged.connect(self.run_task)
-
-        search_button = QPushButton('Search', self)
-        search_button.move(20, 80)
-        search_button.clicked.connect(self.on_click)
-
-        self._output_text_box = QTextEdit(self)
-        self._output_text_box.move(120, 120)
-        self._output_text_box.resize(880, 140)
-        self._output_text_box.setReadOnly(True)
+        self._start_path_input = self._start_path_input()
+        search_button = self._search_button()
+        self._output_text_box = self._output_text_box()
 
         layout = QFormLayout()
         layout.addRow("Provide the default search path here.", self._start_path_input)
+
         layout.addRow("Search button", search_button)
         layout.addRow("Output", self._output_text_box)
 
@@ -60,9 +72,10 @@ class MainWindow(QMainWindow):
 
     def run_task(self):
         if self._thread_count < self._max_thread_count:
-            self._thread_count +=1
+            self._thread_count += 1
             runnable = PathSearchRunnable(self._thread_count)
             self._thread_pool.start(runnable)
+
     @pyqtSlot()
     def on_click(self):
         """Button Action function"""
