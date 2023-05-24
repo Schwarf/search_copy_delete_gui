99274e8785b8001a8a23046ae3dc21a2bcaa146a
Andreas Scharf <andreas.b.scharf@gmail.com>
2023-05-24 13:22:07 +0200

    Cleanup.

--- main_window_layout/q_main_window.py
+++ main_window_layout/q_main_window.py
@@ -1,9 +1,8 @@
-from typing import Optional, List
+from typing import List
 
-from PyQt5.QtCore import QThread, QRegExp, QThreadPool, QCoreApplication
-from PyQt5.QtGui import QRegExpValidator, QColor
-from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QLineEdit, QFormLayout, QTextEdit, QVBoxLayout, \
-    QDialogButtonBox, QHBoxLayout, QLabel
+from PyQt5.QtCore import QRegExp, QThreadPool, QCoreApplication
+from PyQt5.QtGui import QRegExpValidator
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QWidget, QLineEdit, QFormLayout, QTextEdit, QHBoxLayout, QLabel
 
 from q_misc import append_text_in_color
 from q_runnable import PathSearchRunnable, ThreadCounter
@@ -15,7 +14,7 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-        self._title = 'Search only'
+        self._title = 'Search for files with pattern in directories'
         self._left = 10
         self._top = 10
         self._width = 1920
@@ -30,31 +29,32 @@ class MainWindow(QMainWindow):
         self._thread_counter.thread_count_changed.connect(lambda count: print("Thread count is: ", count))
         self._thread_pool = QThreadPool.globalInstance()
 
-    def _default_path(self) -> QLineEdit:
-        start_path_input = QLineEdit(self)
-        start_path_input.setValidator(self._start_path_validator)
-        start_path_input.textChanged.connect(self.run_search)
-        return start_path_input
+    def setup_search_path_input(self) -> QLineEdit:
+        default_path = QLineEdit(self)
+        default_path.setValidator(self._start_path_validator)
+        default_path.textChanged.connect(self.run_search)
+        return default_path
 
-    def _exit_button_clicked(self):
-        QCoreApplication.instance().quit()
-
-    def _exit_button(self) -> QPushButton:
+    def setup_exit_button(self) -> QPushButton:
         exit_button = QPushButton('Exit', self)
-        #exit_button.move(20, 80)
-        exit_button.clicked.connect(self._exit_button_clicked)
+        # exit_button.move(20, 80)
+        exit_button.clicked.connect(QCoreApplication.instance().quit)
         return exit_button
 
-    def _found_folders(self) -> QTextEdit:
+    def setup_search_output(self) -> QTextEdit:
         output_text_box = QTextEdit(self)
         output_text_box.setReadOnly(True)
         return output_text_box
 
-    def _file_pattern_text_box(self) -> QLineEdit:
+    def setup_file_pattern_input(self) -> QLineEdit:
         file_pattern_text_box = QLineEdit(self)
         file_pattern_text_box.setValidator(self._sub_path_validator)
-        file_pattern_text_box.textChanged.connect(self.run_search)
         return file_pattern_text_box
+
+    def setup_search_button(self) -> QPushButton:
+        search_button = QPushButton("Search file pattern")
+        search_button.clicked.connect(self.run_search)
+        return search_button
 
     def init_ui(self):
         """Window Geometry"""
@@ -63,35 +63,31 @@ class MainWindow(QMainWindow):
         self.setGeometry(self._left, self._top, self._width, self._height)
         # label
         # Create textbox
-        self._default_path = self._default_path()
-        exit_button = self._exit_button()
-        self._found_folders = self._found_folders()
-        self._file_pattern_text_box = self._file_pattern_text_box()
+        self._search_path_input = self.setup_search_path_input()
+        self._search_output = self.setup_search_output()
+        self._file_pattern_input = self.setup_file_pattern_input()
 
-        search_button = QPushButton("Search file pattern")
-        search_button.clicked.connect(self.run_search)
-        file_pattern_label = QLabel("Provide file pattern:")
-        self._file_pattern_line_edit = QLineEdit()
-        file_pattern_widget = QWidget()
-        file_pattern_layout = QHBoxLayout(file_pattern_widget)
-        file_pattern_layout.addWidget(file_pattern_label)
-        file_pattern_layout.addWidget(self._file_pattern_line_edit)
+        exit_button = self.setup_exit_button()
+        self._search_button = self.setup_search_button()
+
 
         outer_layout = QFormLayout()
-        outer_layout.addRow("Provide the default search path here.", self._default_path)
-        outer_layout.addRow(file_pattern_widget, search_button)
-        outer_layout.addRow("Folders found in given path", self._found_folders)
+        outer_layout.addRow("Provide the default search path here.", self._search_path_input)
+        outer_layout.addRow(self._search_button, self._file_pattern_input)
+        outer_layout.addRow("Folders found in given path", self._search_output)
         outer_layout.addRow("Exit button", exit_button)
         widget.setLayout(outer_layout)
         self.setCentralWidget(widget)
         self.show()
 
     def run_search(self):
+        sender = self.sender()
         if self._thread_counter.count < self._max_thread_count:
-            if not self._file_pattern_line_edit.text():
-                self._runnable = PathSearchRunnable(self._thread_counter, self._default_path.text())
-            else:
-                self._runnable = PathSearchRunnable(self._thread_counter, self._default_path.text(), self._file_pattern_line_edit.text())
+            if sender == self._search_button:
+                self._runnable = PathSearchRunnable(self._thread_counter, self._search_path_input.text(),
+                                                    self._file_pattern_input.text())
+            elif sender == self._search_path_input:
+                self._runnable = PathSearchRunnable(self._thread_counter, self._search_path_input.text())
             self._runnable.signal_search_finished.search_result_ready.connect(self._on_search_button_clicked)
             self._thread_pool.start(self._runnable)
 
@@ -102,6 +98,6 @@ class MainWindow(QMainWindow):
             color = 'red'
             search_results = ['Invalid path!!!']
         else:
-            self._found_folders.clear()
+            self._search_output.clear()
         for result in search_results:
-            append_text_in_color(self._found_folders, result, color)
+            append_text_in_color(self._search_output, result, color)
